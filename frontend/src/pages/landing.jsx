d97c31f21ca0ae0acb324150a5dba1d46c58a926
XuanGiaHanNguyen <han316@usf.edu>
2025-02-15 14:17:35 -0500

    basic layout

--- frontend/src/pages/landing.jsx
+++ frontend/src/pages/landing.jsx
@@ -4,8 +4,27 @@ import React, { useState, useEffect } from "react";
 const Landing = () => {
    
     return(
-        <div className='bg-black w-full h-screen'>
+        <div className='w-full h-screen flex flex-row'>
+            <div className='flex-1 flex items-center justify-center flex-col'  style={{backgroundColor: '#567C8D'}}>
+                <h1>
+                    Typer Car 
+                </h1>
+                <p>
+                    Speed isn’t just for the track—burn rubber on the keyboard and leave typos in the dust! 
+                </p>
+                <button>
+                    Start Now 
+                </button>
+            </div>
+            <div className='bg-white flex-1 flex flex-col'>
+                <div className='flex-3' style={{backgroundColor: '#C8D9E6'}}>
 
+                </div>
+                <div className='flex-1 flex flex-col'>
+                    <div className='flex-1' style={{backgroundColor: '#bbbdb9'}}/>
+                    <div className='flex-3' style={{backgroundColor: '#c9d4c1'}}/>
+                </div>
+            </div>
         </div>
     );
 }
