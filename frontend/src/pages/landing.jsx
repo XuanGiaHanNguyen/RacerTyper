1d3ebe119dcaa502a1dcfe7e784c4200bdb68dc4
XuanGiaHanNguyen <han316@usf.edu>
2025-02-14 21:22:05 -0500

    background

--- frontend/src/pages/landing.jsx
+++ frontend/src/pages/landing.jsx
@@ -5,6 +5,9 @@ import landingpg from '../assets/main/landing.png';
 import Car1 from '../assets/main/Car1.png'; 
 import Bus from '../assets/main/Bus.png'; 
 import Car from '../assets/main/car.png'; 
+import bush from '../assets/main/bush.png'; 
+import bench from '../assets/main/bench.png'; 
+import tree from '../assets/main/tree.png'; 
 
 
 const Landing = () => {
@@ -52,8 +55,13 @@ const Landing = () => {
                 style={{ backgroundColor: "#f5c6ac" }}
                 >
                 Start Now
-                </button>
+            </button>
             <div className='relative flex w-full mt-20 pb-48'>
+                <img src={tree} alt="tree" className='absolute bottom-0 w-56' style={{zIndex: 2 }} />
+                <img src={bush} alt="bush" className='absolute bottom-0 w-32' style={{ zIndex: 1 }} />
+                <img src={bush} alt="bush" className='absolute bottom-0 left-10 w-32' style={{ zIndex: 1 }} />
+                <img src={bush} alt="bush" className='absolute bottom-0 right-0 w-48' style={{ zIndex: 1 }} />
+                <img src={bench} alt="bench" className='absolute bottom-0 right-1 w-40' style={{  zIndex: 1 }} />
                 <img src={Car1} alt="Car1" className='absolute bottom-0 w-40' style={{ left: `${carPosition}px`, zIndex: 2 }} />
                 <img src={Bus} alt="Bus" className='absolute bottom-0 w-48' style={{ left: `${busPosition}px`, zIndex: 1 }} />
                 <img src={Car} alt="Car2" className='absolute bottom-0 w-40' style={{ left: `${Car2Position}px`, zIndex: 1 }} />
